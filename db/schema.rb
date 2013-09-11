366de6ccb6ea27dbfec3517191b4a4fb7f01188c
Richard Davis <rdavis.bacs@gmail.com>
2013-09-11 16:04:08 -0400

    customer spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130911193624) do
+ActiveRecord::Schema.define(version: 20130911195836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20130911193624) do
     t.integer  "cost",          null: false
     t.string   "art_medium",    null: false
     t.integer  "artist_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "customers", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone_number"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
